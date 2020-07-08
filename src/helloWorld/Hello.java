122e8a634aacc3cb47ca674b247745305147eaea
Matthew Kaye <hello@mattycodes.co.uk>
2020-07-08 19:41:20 +0100

    Adding integer variable

--- src/helloWorld/Hello.java
+++ src/helloWorld/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("Hello, Clarise. (I'm not too imaginative.)");
+		
+		int myInteger = (10 + 5) + (2 * 10);
+		System.out.println(myInteger);
 	}
 
 }
