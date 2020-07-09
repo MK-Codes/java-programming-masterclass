5c544685149146901fef3cbb33644689d5d93ca6
Matthew Kaye <hello@mattycodes.co.uk>
2020-07-09 06:28:19 +0100

    Primitive types - short, long, byte

--- src/byteShortIntLong/Main.java
+++ src/byteShortIntLong/Main.java
@@ -12,6 +12,21 @@ public class Main {
 		System.out.println("Integer max val: " + myMaxIntValue);
 		System.out.println("Busted min val (underflow): " + (myMinIntValue - 1));
 		System.out.println("Busted max val: (overflow)" + (myMaxIntValue + 1));
+		
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+		System.out.println("Byte min val: " + myMinByteValue);
+		System.out.println("Byte max val: " + myMaxByteValue);
+
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		System.out.println("Short min val: " + myMinShortValue);
+		System.out.println("Short max val: " + myMaxShortValue);
+
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+		System.out.println("Long min val: " + myMinLongValue);
+		System.out.println("Long max val: " + myMaxLongValue);
 	}
 
 }
