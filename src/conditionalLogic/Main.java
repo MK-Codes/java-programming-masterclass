deb27ed74f94e1c9c806e2dd1e4df40b6bb6a13a
MK <hello@mattycodes.co.uk>
2020-07-11 10:33:16 +0100

    Conditional logic - if statement

---
+++ src/conditionalLogic/Main.java
@@ -0,0 +1,13 @@
+package conditionalLogic;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		boolean isAlien = false;
+		if (isAlien == false) {
+			System.out.println("It is not an alien.");
+		}
+	}
+
+}
