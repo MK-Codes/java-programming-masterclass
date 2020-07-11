3988e9c7d61d7f8c4fcde7e8be18ed83358602fd
MK <hello@mattycodes.co.uk>
2020-07-11 10:23:21 +0100

    abbreviating operators

--- src/operators/Main.java
+++ src/operators/Main.java
@@ -22,6 +22,25 @@ public class Main {
 		
 		result = result % 3; // modulo (remainder)
 		System.out.println("4 % 3 = " + result);
+		
+		// result = result + 1 can be abbreviated
+		result++; // 1 + 1 = 2
+		System.out.println("1 + 1 = " + result);
+		
+		result--; // 2 - 1 = 1
+		System.out.println("2 - 1 = " + result);
+		
+		result += 2;
+		System.out.println("1 + 2 = " + result);
+		
+		result *= 10;
+		System.out.println("3 * 10 = " + result);
+		
+		result /= 3;
+		System.out.println("30 / 3 = " + result);
+		
+		result -= 2;
+		System.out.println("10 - 2 = " + result);
 	}
 
 }
