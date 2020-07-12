ac1c3e713ebd16d9189a69c0d6bd1b2d40d406e7
Matthew Kaye <hello@mattycodes.co.uk>
2020-07-12 20:46:50 +0100

    Logical AND operator

--- src/conditionalLogic/Main.java
+++ src/conditionalLogic/Main.java
@@ -8,6 +8,17 @@ public class Main {
 		if (isAlien == false) {
 			System.out.println("It is not an alien.");
 		}
+		
+		int topScore = 100;
+		
+		if (topScore >= 100) {
+			System.out.println("You got the high score.");
+		}
+		int secondTopScore = 60;
+		
+		if (topScore > secondTopScore && topScore < 100) {
+			System.out.println("Greater than second top score, less than 100");
+		}
 	}
 
 }
