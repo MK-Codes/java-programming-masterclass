a5b2cb9551f49684d9f92cb3cdbfd25eb96e303b
Matthew Kaye <hello@mattycodes.co.uk>
2020-07-15 22:06:13 +0100

    Whitespace and indenting

---
+++ src/statementsWhitespaceAndIndenting/Main.java
@@ -0,0 +1,20 @@
+package statementsWhitespaceAndIndenting;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int myVar = 50;
+		myVar++;
+		myVar--;
+		System.out.println("This is a test");
+		
+		System.out.println("This is " +
+				"a test.");
+		
+		int anotherVariable = 50;myVar--;System.out.println("Still fine!");
+int i     =        50         ;String 
+ a   = "aa"; // This is fine but oh goodness never do it
+	}
+
+}
