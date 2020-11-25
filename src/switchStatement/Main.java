dc81ebeb6cd70624c97fb819e65dacde88942653
Matthew Kaye <hello@mattycodes.co.uk>
2020-11-25 21:50:37 +0000

    Switch - additional challenge for days of week

--- src/switchStatement/Main.java
+++ src/switchStatement/Main.java
@@ -63,6 +63,47 @@ public class Main {
 			break;
 		default:
 				System.out.println("Nah not up to May");
+				
+		}
+		// Calling next challenge to test
+		printDayOfTheWeek(0);
+		printDayOfTheWeek(3);
+		printDayOfTheWeek(4);
+		printDayOfTheWeek(5);
+		printDayOfTheWeek(72);
+	}
+
+	// Extra challenge!
+	// Write a method with the name printDayOfTheWeek that has one parameter of type int and name it day
+	// The method should not return any value
+	// Using a switch statement, print "Sunday", "Monday", etc., if the int parameter "day"is 0, 1, etc., otherwise print "Invalid day"
+
+	public static void printDayOfTheWeek(int day) {
+
+		switch (day) {
+		case 0:
+			System.out.println("Sunday");
+			break;
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+			default:
+				System.out.println("Invalid day");
 		}
 	}
 }
